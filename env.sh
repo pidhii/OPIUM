2667b0faa09b16410e0bce8c6963a1ab0258d251
Ivan Pidhurskyi <ivanpidhurskyi1997@gmail.com>
2020-02-17 02:53:41 +0200

    Add pkg-config file: "opium.pc".

--- env.sh
+++ env.sh
@@ -1,9 +1,11 @@
 p=`realpath ${1:?path not specified}`
 
-echo "PATH += $p/bin"
-echo "OPIUM_ROOT = $p"
-echo "OPIUM_PATH = $p/lib/opium"
+#echo "PATH += $p/bin"
+#echo "OPIUM_ROOT = $p"
+#echo "OPIUM_PATH = $p/lib/opium"
+#echo "PKG_CONFIG_PATH = $p/lib/pkgconfig"
 
 export PATH=$p/bin:$PATH
 export OPIUM_ROOT=$p
 export OPIUM_PATH=$p/lib/opium
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH:+${PKG_CONFIG_PATH}:}$p/lib/pkgconfig
